e999321289d442a3d5dd6c99c179d57bd92415f1
rabbanibhuyain <rabbani.bhuyain@outlook.com>
2017-06-21 21:00:05 -0400

    formatDateHeader format fixed

--- src/app/shared/format-date.pipe.ts
+++ src/app/shared/format-date.pipe.ts
@@ -5,19 +5,8 @@ import * as moment from 'moment';
 export class FormatDate implements PipeTransform {
     transform(value: string): string {
         if (!value) return value;
-//let initialDate : Date = this.getDateFromStr(value);
-        let initialDate =  moment(value);
-      
-        return  moment().format('ddd, MMMM DD, YYYY') ;
+        return  moment(value).format('ddd, MMMM DD, YYYY') ;
        
-    }
-
-
-    getDateFromStr(dateStr) {
-        const [datePart, timePart] = dateStr.split(" ");
-        const [year, month, day] = datePart.split("-");
-        const [hour, minute, sec] = timePart.split(":");
-        return new Date(year, month - 1, day, hour, minute, sec);
     }
 
 }
